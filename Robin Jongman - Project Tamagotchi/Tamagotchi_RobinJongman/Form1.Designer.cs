d531849f78274ab42ab868cf654c43b25e73138f
Robin-47 <robinjongmannova@gmail.com>
2026-09-10 15:09:54 +0200

    Add files via upload

---
+++ Robin Jongman - Project Tamagotchi/Tamagotchi_RobinJongman/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Tamagotchi_RobinJongman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.drinkBar = new System.Windows.Forms.ProgressBar();
+            this.hongerBar = new System.Windows.Forms.ProgressBar();
+            this.welzijnBar = new System.Windows.Forms.ProgressBar();
+            this.drinken = new System.Windows.Forms.Button();
+            this.eten = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::Tamagotchi_RobinJongman.Properties.Resources.perfect;
+            this.pictureBox1.Location = new System.Drawing.Point(30, 106);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(760, 340);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(349, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 25);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(619, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 25);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 25);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "1";
+            // 
+            // drinkBar
+            // 
+            this.drinkBar.BackColor = System.Drawing.Color.Cyan;
+            this.drinkBar.ForeColor = System.Drawing.Color.Red;
+            this.drinkBar.Location = new System.Drawing.Point(551, 57);
+            this.drinkBar.Name = "drinkBar";
+            this.drinkBar.Size = new System.Drawing.Size(239, 23);
+            this.drinkBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.drinkBar.TabIndex = 15;
+            // 
+            // hongerBar
+            // 
+            this.hongerBar.BackColor = System.Drawing.Color.Lime;
+            this.hongerBar.Location = new System.Drawing.Point(30, 57);
+            this.hongerBar.Name = "hongerBar";
+            this.hongerBar.Size = new System.Drawing.Size(239, 23);
+            this.hongerBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.hongerBar.TabIndex = 14;
+            // 
+            // welzijnBar
+            // 
+            this.welzijnBar.BackColor = System.Drawing.Color.Red;
+            this.welzijnBar.Location = new System.Drawing.Point(286, 57);
+            this.welzijnBar.Name = "welzijnBar";
+            this.welzijnBar.Size = new System.Drawing.Size(239, 23);
+            this.welzijnBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.welzijnBar.TabIndex = 13;
+            // 
+            // drinken
+            // 
+            this.drinken.BackColor = System.Drawing.Color.Transparent;
+            this.drinken.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("drinken.BackgroundImage")));
+            this.drinken.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drinken.Location = new System.Drawing.Point(562, 464);
+            this.drinken.Name = "drinken";
+            this.drinken.Size = new System.Drawing.Size(228, 67);
+            this.drinken.TabIndex = 12;
+            this.drinken.Text = "Drinken";
+            this.drinken.UseVisualStyleBackColor = false;
+            this.drinken.Click += new System.EventHandler(this.drinken_Click);
+            // 
+            // eten
+            // 
+            this.eten.BackColor = System.Drawing.Color.Transparent;
+            this.eten.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("eten.BackgroundImage")));
+            this.eten.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eten.Location = new System.Drawing.Point(30, 464);
+            this.eten.Name = "eten";
+            this.eten.Size = new System.Drawing.Size(228, 67);
+            this.eten.TabIndex = 11;
+            this.eten.Text = "Eten";
+            this.eten.UseVisualStyleBackColor = false;
+            this.eten.Click += new System.EventHandler(this.eten_Click);
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.Transparent;
+            this.start.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("start.BackgroundImage")));
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(297, 464);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(228, 67);
+            this.start.TabIndex = 10;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(822, 564);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drinkBar);
+            this.Controls.Add(this.hongerBar);
+            this.Controls.Add(this.welzijnBar);
+            this.Controls.Add(this.drinken);
+            this.Controls.Add(this.eten);
+            this.Controls.Add(this.start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Tamagotchi Tom";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar drinkBar;
+        private System.Windows.Forms.ProgressBar hongerBar;
+        private System.Windows.Forms.ProgressBar welzijnBar;
+        private System.Windows.Forms.Button drinken;
+        private System.Windows.Forms.Button eten;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Robin Jongman - Project Tamagotchi/Tamagotchi_RobinJongman/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tamagotchi_RobinJongman
+{
+    // Robin Jongman
+
+    public partial class Form1 : Form
+    {
+        int score;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Tamagotchi();
+        }
+
+        private void Tamagotchi()
+        {
+            // Stelt de timers in om 60 seconden actief te blijven
+            timer1.Interval = 295;
+            timer1.Tick += Timer1_Tick;
+
+            timer2.Interval = 295;
+            timer2.Tick += Timer2_Tick;
+
+            timer3.Interval = 295;
+            timer3.Tick += Timer3_Tick;
+
+            // Stelt de minimum en maximum waarden van de progressbars in
+            hongerBar.Minimum = 0;
+            hongerBar.Maximum = 100;
+            hongerBar.Value = 100;
+
+            drinkBar.Minimum = 0;
+            drinkBar.Maximum = 100;
+            drinkBar.Value = 100;
+
+            welzijnBar.Minimum = 0;
+            welzijnBar.Maximum = 100;
+            welzijnBar.Value = 100;
+
+            // Stelt de beginwaarde van de labels in
+            label1.Text = "Honger: 100";
+            label2.Text = "Drink: 100";
+            label3.Text = "Welzijn: 100";
+
+            score = 0;
+        }
+
+        // Start de timers wanneer er op de Start-knop wordt geklikt
+        private void start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Start();
+                timer2.Start();
+                timer3.Start();
+            }
+            catch
+            {
+                MessageBox.Show($"Fout bij het starten van de timers: ");
+            }
+        }
+
+        // Voegt 5 toe aan de hongerbar wanneer er op de Eten-knop wordt geklikt
+        private void eten_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                hongerBar.Value = Math.Min(hongerBar.Value + 5, 100);
+                Labels();
+            }
+            catch
+            {
+                MessageBox.Show($"Fout bij het verhogen van de hongerbar: ");
+            }
+        }
+
+        // Voegt 5 toe aan de drinkbar wanneer er op de Drinken-knop wordt geklikt
+        private void drinken_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                drinkBar.Value = Math.Min(drinkBar.Value + 5, 100);
+                Labels();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fout bij het verhogen van de drinkbar: {ex.Message}");
+            }
+        }
+
+        // Verminderd de hongerbalk van timer1 met 1 per tik
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                hongerBar.Increment(-1);
+            }
+            catch
+            {
+                MessageBox.Show($"Fout bij het verminderen van de hongerbar: ");
+            }
+        }
+
+        // Verminderd de drinkbalk van timer2 met 1 per tik
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                drinkBar.Increment(-1);
+            }
+            catch
+            {
+                MessageBox.Show($"Fout bij het verminderen van de drinkbar: ");
+            }
+        }
+
+        // Verminderd de welzijnsbalk van timer3 met 1 per tik
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                // Zorgt ervoor dat de welzijnsbalk daalt met -1 als de hongerbar en drinkbar beide 0 zijn
+                if (hongerBar.Value == 0 && drinkBar.Value == 0)
+                {
+                    welzijnBar.Value = Math.Max(welzijnBar.Value - 1, 0);
+                }
+
+                PictureBox();
+                Labels();
+
+                if (welzijnBar.Value == 0)
+                {
+                    timer1.Stop();
+                    timer2.Stop();
+                    timer3.Stop();
+                    MessageBox.Show($"Tom is Overleden. Eindscore: {score}");
+                    Application.Exit();
+                }
+
+                score++;
+            }
+            catch
+            {
+                MessageBox.Show($"Fout bij het bijwerken van de welzijnsbalk en score:");
+            }
+        }
+
+        // Zorgt ervoor dat de afbeeldingen veranderen afhankelijk van het welzijn
+        private void PictureBox()
+        {
+            if (welzijnBar.Value == 0)
+            {
+                pictureBox1.Image = Properties.Resources.dood;
+            }
+            else if (welzijnBar.Value < 25)
+            {
+                pictureBox1.Image = Properties.Resources.bijnaDood;
+            }
+            else if (welzijnBar.Value < 50)
+            {
+                pictureBox1.Image = Properties.Resources.nietGoed;
+            }
+            else if (welzijnBar.Value < 75)
+            {
+                pictureBox1.Image = Properties.Resources.redelijk;
+            }
+            else
+            {
+                pictureBox1.Image = Properties.Resources.perfect;
+            }
+        }
+
+        // Zorgt ervoor dat de labels geupdatet blijven
+        private void Labels()
+        {
+            label1.Text = $"Honger: {hongerBar.Value}";
+            label2.Text = $"Drink: {drinkBar.Value}";
+            label3.Text = $"Welzijn: {welzijnBar.Value}";
+        }
+    }
+}
